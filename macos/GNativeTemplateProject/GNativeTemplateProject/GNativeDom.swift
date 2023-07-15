91adec5f938d48628d7704df365d51730ce46259
Steven Zack <zigzigcheers@gmail.com>
2023-07-15 12:40:52 +0800

    backup

--- macos/GNativeTemplateProject/GNativeTemplateProject/GNativeDom.swift
+++ macos/GNativeTemplateProject/GNativeTemplateProject/GNativeDom.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+class GNativeDom {
+    var tagName: String = ""
+    var attributes: [String: String] = [:]
+    var children: [GNativeDom] = []
+    var innerText: String = ""
+    
+    init(_ s: String) {
+        
+    }
+}
